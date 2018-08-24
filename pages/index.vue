dd0e9bea1af22459a8d4516c9a7d86d428700757
nobuaki.yamaji <nobuaki.yamaji@monopos.co.jp>
2018-08-25 04:27:33 +0900

    design

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
     <section class="container">
         <div class="columns is-multiline">
-            <div v-for="(item, i) in instructor_list" v-bind:key='i' class='column is-2'>
-                <img v-bind:src="'https://cdn.slidesharecdn.com/profile-photo-' + i + '-96x96.jpg?cb=1534998396'" />
-                <nuxt-link :to="{ path: 'instructors/'+ i }" class="button">{{ i }}</nuxt-link>
+            <div v-for="(item, i) in instructor_list" v-bind:key='i' class='column is-3'>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img v-bind:src="'https://cdn.slidesharecdn.com/profile-photo-' + i + '-96x96.jpg?cb=1534998396'">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">{{i}}</p>
+                                <nuxt-link :to="{ path: 'instructors/'+ i }" class="button is-link">気になる</nuxt-link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <!-- 適当なコメント -->
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                        <a href="#">#css</a> <a href="#">#responsive</a>
+                        <br>
+                        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
